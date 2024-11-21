d4d9ac90796d620bc7992aa52c14547afbeead77
Dmitri_Simakin <dima.simakin00@mail.ru>
2024-11-21 15:26:15 +0300

    add tail and complete file reading

--- matrix.cpp
+++ matrix.cpp
@@ -31,7 +31,7 @@ void print_matrix(std::complex<double> *matrix, int n) {
 }
 
 class Point3D {
-    public:
+public:
     double x, y, z;
 
     Point3D() {};
@@ -48,6 +48,10 @@ class Point3D {
 
     Point3D operator-(Point3D P2) const {
         return Point3D(x - P2.x, y - P2.y, z - P2.z);
+    }
+
+    Point3D operator+(Point3D P2) const {
+        return Point3D(x + P2.x, y + P2.y, z + P2.z);
     }
 
     Point3D operator/(double num) const {
@@ -72,12 +76,11 @@ Point3D operator+(const Point3D& P1, const Point3D& P2) {
         return Point3D(P1.x + P2.x, P1.y + P2.y, P1.z + P2.z);
 }
 
-double DotProd_Point(Point3D& P1, Point3D& P2)
-{
-    double res;
-    res = P1.x * P2.x + P1.y * P2.y + P1.z * P2.z;
-    return res;
+bool operator!=(const Point3D& P1, const Point3D& P2) {
+    return ( (P1.x == P2.x && P1.y == P2.y && P1.z == P2.z) ? false : true);
 }
+
+double DotProd_Point(Point3D& P1, Point3D& P2) { return P1.x * P2.x + P1.y * P2.y + P1.z * P2.z; }
 
 Point3D VecProd_Point(Point3D& P1, Point3D& P2)
 {
@@ -88,19 +91,19 @@ Point3D VecProd_Point(Point3D& P1, Point3D& P2)
     return res;
 }
 
-
-
 class Frame {
-    public:
-    Point3D A, B, C, D;
+public:
+    vector<Point3D> four_points{}; // A, B, C, D;
     double square;
     Point3D norm;
 
+    Frame() {};
+
     Frame(const Point3D& _A, const Point3D& _B, const Point3D& _C, const Point3D& _D) {
-        this->A = _A;
-        this->B = _B;
-        this->C = _C;
-        this->D = _D;
+        this->four_points.push_back(_A);
+        this->four_points.push_back(_B);
+        this->four_points.push_back(_C);
+        this->four_points.push_back(_D);
 
         Point3D d1 = _A - _C;
         Point3D d2 = _B - _D;
@@ -108,13 +111,30 @@ class Frame {
         square = abs(VecProd_Point(d1, d2)) / 2;
         norm = VecProd_Point(d1, d2) / (2*square);
     }
+
+    void add_point(const Point3D& P)
+    {
+        if(four_points.size() == 4) {
+            cout << "Warning! Full size of frame.\n";
+            exit;
+        }
+        four_points.push_back(P);
+    }
+    void add_point_coord(const double& x, const double& y,const double& z)
+    {
+        if(four_points.size() == 4) {
+            cout << "Warning! Full size of frame.\n";
+            exit;
+        }
+        four_points.push_back(Point3D(x, y, z));
+    }
 };
 
-void init(std::string path, vector<Frame>& frames, vector<int>& up_ind, vector<int>& down_ind) {
+void init(std::string path, vector<Frame>& frames, double par) {  // vector<int>& up_ind, vector<int>& down_ind
 
     std::string line;
     double x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4;
- 
+    
     std::ifstream in(path); // окрываем файл для чтения
     if (in.is_open())
     {
@@ -127,6 +147,8 @@ void init(std::string path, vector<Frame>& frames, vector<int>& up_ind, vector<i
         //         
         //     }
         // }
+
+        vector<Frame> sled;
         while (std::getline(in, line)) {
             std::istringstream ss(line);
             ss >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> x3 >> y3 >> z3 >> x4 >> y4 >> z4;
@@ -134,23 +156,54 @@ void init(std::string path, vector<Frame>& frames, vector<int>& up_ind, vector<i
             Point3D B(x2, y2, z2);
             Point3D C(x3, y3, z3);
             Point3D D(x4, y4, z4);
-
-
-            frames.push_back(Frame(A, B, C, D));
+            vector<Point3D> vec_points{A, B, C, D};
+            // cout << A << B << C << D;
+            Frame frame_tmp(A,B,C,D);
+            frames.push_back(frame_tmp);  // Frame(A, B, C, D)
 
             double xs[4] = {x1, x2, x3, x4};
-            int count = 0;
+            bool x_flag[4] = {false};
+            int counter = 0;
             for (int i = 0; i < 4; ++i) {
                 if (abs(xs[i] - 1.0) < 1e-6) {
-                    count++;
+                    x_flag[i] = true;
+                    counter++;
                 }
             }
-
-            if (count == 2) {
-
+            if (counter== 2) {
+                // проверка, что это не треугольник.
+                if (A != B && B != C && C != D && D != A && B != D && A != C) {
+                    Frame frame_sled;
+                    for(int i=0; i < 4; ++i) {
+                        if (x_flag[i] == true) {
+                            frame_sled.add_point(vec_points[i]);
+                            frame_sled.add_point_coord(vec_points[i].x + par, vec_points[i].y, vec_points[i].z);
+                        }
+                    }
+                    sled.push_back(frame_sled);
+                }
             }
-
         }
+
+        for (int i=0; i<sled.size(); ++i){
+            for(int j=0; j<4; ++j){
+                cout << sled[i].four_points[j];
+            }
+            cout << endl;
+        }
+        // Создание дополнительных слоев для хвоста, но они не нужны, достаточно одного.
+        // int size_sled = sled.size();
+        // for(int i=0; i < 20; ++i){
+        //     Frame frame_sled;
+        //     for(int j=0; j < sled.size()-i*size_sled; ++j) {
+        //         frame_sled.add_point(sled[j].four_points[1]);
+        //         frame_sled.add_point_coord(sled[j].four_points[1].x + 1, sled[j].four_points[1].y, sled[j].four_points[1].z);
+        //         frame_sled.add_point(sled[j].four_points[3]);
+        //         frame_sled.add_point_coord(sled[j].four_points[3].x + 1, sled[j].four_points[3].y, sled[j].four_points[3].z);
+        //     }
+        //     sled.push_back(frame_sled);
+        // }
+
     }
     in.close();     // закрываем файл
 }
@@ -205,14 +258,28 @@ int main() {
     //     free(b2);
     //     free(Ipvt);
     // }
-    //Point3D P1(1.5, 1.5, 10.3);
-    //Point3D P2(0.5, 3.0, 11.0);
-    //cout << P1 - P2;
+    Point3D P1(1.0, 0.0, 0.0);
+    Point3D P2(1.0, 0.0, 1.0);
+    Point3D P3(0.0, 0.0, 1.0);
+    Point3D P4(0.0, 0.0, 0.0);
+    
+    Frame frame1 (P1, P2, P3, P4);
 
-    vector<Frame> frames;
-    vector<int> up_ind;
-    vector<int> down_ind;
-    init("wing_10_20.dat", frames, up_ind, down_ind);
+    // vector<Frame> frames;
+    // double par = 10.0;
+    // init("wing_2.dat", frames, par);
+
+
+    cout << frame1.norm;
+    cout << frame1.square << endl;
+
+
+
+
+    // vector<Frame> frames;
+    // vector<int> up_ind;
+    // vector<int> down_ind;
+    // init("wing_10_20.dat", frames, up_ind, down_ind);
 
     return 0;
 }
